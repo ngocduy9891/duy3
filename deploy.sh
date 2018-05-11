fb9cb36a48c1e8ea12f72e733ccdf2b67c8f9a71
ngoc duy tran <36229133+ngocduy9891@users.noreply.github.com>
2018-05-11 13:55:19 +0700

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -5,13 +5,13 @@ SHA1=$1
 
 # Push image to ECR
 $(aws ecr get-login --region <region>)
-docker push <account-id>.dkr.ecr.<region>.amazonaws.com/<repo>:$SHA1
+docker push 012881927014.dkr.ecr.ap-southeast-1.amazonaws.com/docker:$SHA1
 
 # Create new Elastic Beanstalk version
-EB_BUCKET=<project>-deploy-bucket
+EB_BUCKET=ngocduy-deploy-bucket
 DOCKERRUN_FILE=$SHA1-Dockerrun.aws.json
 sed "s/<TAG>/$SHA1/" < Dockerrun.aws.json.template > $DOCKERRUN_FILE
-aws s3 cp $DOCKERRUN_FILE s3://$EB_BUCKET/$DOCKERRUN_FILE --region <region>
-aws elasticbeanstalk create-application-version --application-name <project> \
+aws s3 cp $DOCKERRUN_FILE s3://$EB_BUCKET/$DOCKERRUN_FILE --region ap-southeast-1
+aws elasticbeanstalk create-application-version --application-name ngocduy \
     --version-label $SHA1 --source-bundle S3Bucket=$EB_BUCKET,S3Key=$DOCKERRUN_FILE \
-    --region <region>
+    --region ap-southeast-1
